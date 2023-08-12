05cff9e4ad22272e7c6486d863bf2661acdedbcf
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2023-08-12 09:39:01 +0530

    initial project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Lottie from 'react-lottie';
+import styled from 'styled-components';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import './App.css';
 
+const TitleText = styled.p`
+  font-size: 50px;
+  color: #2D4356;
+  font-weight: 800;
+  margin: 0px;
+`;
+const SubTitleText = styled.p`
+  color: #435B66;
+  font-size: 18px;
+  line-height: 27px;
+`;
+const FormConatiners = styled.div`
+  
+`
+
 function App() {
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: "https://lottie.host/?file=84d418cb-699d-40fb-9825-fd2259783116/tSuZY5yz2E.json",
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg" sx={{ marginTop: '30px' }}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Stack>
+            <TitleText>CrossCarat</TitleText>
+            <SubTitleText>
+              CrossCarat bridges the gap between EVM-based blockchains, enabling seamless and secure cross-chain communication. Instantly connect and collaborate across networks, opening new doors for decentralized innovation. Experience frictionless data exchange, tamper-proof messaging, and limitless possibilities. Unite EVM blockchains with CrossCarat.
+            </SubTitleText>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          {/* <Lottie options={defaultOptions}
+            height={400}
+            width={400} /> */}
+        </Grid>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
